97d9ec7576a6bd783b59154535df4487767ca887
Amy <aimeizheng@outlook.com>
2019-07-03 11:20:33 +0800

    test

--- src/com/ecut/LambdaTest.java
+++ src/com/ecut/LambdaTest.java
@@ -1,9 +1,12 @@
 package com.ecut;
 
 /**
- * @auther zhengamy
- * @date 2019-07-03 11:18
+ * @auther Amy
+ * @date 2019-07-03 11:20
  * @description:
  */
 public class LambdaTest {
+    public static void main(String[] args) {
+
+    }
 }
