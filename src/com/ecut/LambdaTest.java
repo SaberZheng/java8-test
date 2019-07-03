2f3a0194a120e69953ce2a3300d5b37112f33cef
Amy <aimeizheng@outlook.com>
2019-07-03 14:44:39 +0800

    test

--- src/com/ecut/LambdaTest.java
+++ src/com/ecut/LambdaTest.java
@@ -1,12 +1,38 @@
 package com.ecut;
 
 /**
- * @auther Amy
+ * @author  Amy
  * @date 2019-07-03 11:20
- * @description:
+ * @description: Lambda表达式测试类
  */
 public class LambdaTest {
-    public static void main(String[] args) {
 
+    /**
+     * 函数式接口：只定义单一的抽象方法的接口
+     */
+    @FunctionalInterface
+    interface ShowNum {
+        void show(int num);
+    }
+
+    @FunctionalInterface
+    interface SayHello {
+        void say(String message);
+    }
+
+    public static void main(String[] args) {
+        //通过lambda表达式实现接口
+        SayHello sayHello = s -> System.out.println("hello " + s);
+        sayHello.say("lambda");
+        //通过lambda表达式实现接口
+        ShowNum showNum = i -> System.out.println(i);
+        //Lambda可以作为参数来传递给函数
+        LambdaTest.print(showNum, 1);
+    }
+
+    private static void print(ShowNum showNum, int num) {
+        showNum.show(num);
     }
 }
+
+
